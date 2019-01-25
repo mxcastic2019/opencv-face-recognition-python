06144a5131c8c5834c2a7373861ef2fffee88ca5
tianwentao123 <wodeshijie619@qq.com>
2019-01-25 18:35:31 +0800

    0125ready
    update

--- 天气报告3.py
+++ 天气报告3.py
@@ -1,19 +1,67 @@
+import numpy as np
+import cv2
+import random
+face_cascade = cv2.CascadeClassifier('C:\chess\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:\chess\haarcascade_eye.xml')
+cap=cv2.VideoCapture(0)
+while(True):
+    ret,frame = cap.read()    
+    img = frame
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            cv2.imshow('roi_color',roi_color)
+            print('hahaha')
+            cv2.imwrite('d:/Documents/GitHub/opencv-face-recognition-python/test-data/face.jpg',roi_color)
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('img',img)
+    if cv2.waitKey(1000)&0xFF==ord('s'):
+        break
+cv2.destroyAllWindows()
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+resp=urlopen('http://www.weather.com.cn/weather/101020100.shtml')
+soup=BeautifulSoup(resp,'html.parser')
+tagDate=soup.find('ul',class_="t clearfix")
+dates=tagDate.h1.string
 
-# coding: utf-8
+tagToday=soup.find('p',class_="tem")
+try:
+	temperatureHigh=tagToday.span.string
+except AttributeError as e:
+	temperatureHigh=tagToday.find_next('p',class_="tem").span.string
 
-# Face Recognition with OpenCV
+temperatureLow=tagToday.i.string
+weather=soup.find('p',class_="wea").string
 
-# To detect faces, I will use the code from my previous article on [face detection](https://www.superdatascience.com/opencv-face-detection/). So if you have not read it, I encourage you to do so to understand how face detection works and its Python coding. 
+tagWind=soup.find('p',class_="win")
+winL=tagWind.i.string
 
-# ### Import Required Modules
-
-# Before starting the actual coding we need to import the required modules for coding. So let's import them first. 
-# 
-# - **cv2:** is _OpenCV_ module for Python which we will use for face detection and face recognition.
-# - **os:** We will use this Python module to read our training directories and file names.
-# - **numpy:** We will use this module to convert Python lists to numpy arrays as OpenCV face recognizers accept numpy arrays.
-
-# In[1]:
+import datetime 
+today = datetime.date.today() 
+print ("Year: %d" % today.year )
+print ("Month: %d" % today.month )
+print ("Day: %d" % today.day )
+print ("Weekday: %d" % today.weekday()) # Day of week Monday = 0, Sunday = 6 
+print ("ISO Weekday: %d" % today.isoweekday()) # Day of week Monday = 1, Sunday = 7 
+print ("ISO Format: %s" % today.isoformat()) # YYYY-MM-DD format 
+print ("ISO Calendar: %s" % str(today.isocalendar())) # Tuple of (ISO year, ISO week number, ISO weekday) 
+print (today.strftime("%Y/%m/%d"))
+familyID=0
+print('上海天气：')
+print('今天是：'+dates)
+print('风级：'+winL)
+print('最低温：'+temperatureLow)
+print('最高温：'+temperatureHigh)
+print('天气：'+weather)
 
 #import OpenCV module
 import cv2
@@ -53,43 +101,6 @@ import numpy as np
 #there is no label 0 in our training data so subject name for index/label 0 is empty
 subjects = ["", "tianwentao", "mayunfei","jiamo"]
 
-# ### Prepare training data
-
-# You may be wondering why data preparation, right? Well, OpenCV face recognizer accepts data in a specific format. It accepts two vectors, one vector is of faces of all the persons and the second vector is of integer labels for each face so that when processing a face the face recognizer knows which person that particular face belongs too. 
-# 
-# For example, if we had 2 persons and 2 images for each person. 
-# 
-# ```
-# PERSON-1    PERSON-2   
-# 
-# img1        img1         
-# img2        img2
-# ```
-# 
-# Then the prepare data step will produce following face and label vectors.
-# 
-# ```
-# FACES                        LABELS
-# 
-# person1_img1_face              1
-# person1_img2_face              1
-# person2_img1_face              2
-# person2_img2_face              2
-# ```
-# 
-# 
-# Preparing data step can be further divided into following sub-steps.
-# 
-# 1. Read all the folder names of subjects/persons provided in training data folder. So for example, in this tutorial we have folder names: `s1, s2`. 
-# 2. For each subject, extract label number. **Do you remember that our folders have a special naming convention?** Folder names follow the format `sLabel` where `Label` is an integer representing the label we have assigned to that subject. So for example, folder name `s1` means that the subject has label 1, s2 means subject label is 2 and so on. The label extracted in this step is assigned to each face detected in the next step. 
-# 3. Read all the images of the subject, detect face from each image.
-# 4. Add each face to faces vector with corresponding subject label (extracted in above step) added to labels vector. 
-# 
-# **[There should be a visualization for above steps here]**
-
-# Did you read my last article on [face detection](https://www.superdatascience.com/opencv-face-detection/)? No? Then you better do so right now because to detect faces, I am going to use the code from my previous article on [face detection](https://www.superdatascience.com/opencv-face-detection/). So if you have not read it, I encourage you to do so to understand how face detection works and its coding. Below is the same code.
-
-# In[3]:
 
 #function to detect face using OpenCV
 def detect_face(img):
@@ -196,29 +207,6 @@ def prepare_training_data(data_folder_path):
     return faces, labels
 
 
-# I have defined a function that takes the path, where training subjects' folders are stored, as parameter. This function follows the same 4 prepare data substeps mentioned above. 
-# 
-# **(step-1)** On _line 8_ I am using `os.listdir` method to read names of all folders stored on path passed to function as parameter. On _line 10-13_ I am defining labels and faces vectors. 
-# 
-# **(step-2)** After that I traverse through all subjects' folder names and from each subject's folder name on _line 27_ I am extracting the label information. As folder names follow the `sLabel` naming convention so removing the  letter `s` from folder name will give us the label assigned to that subject. 
-# 
-# **(step-3)** On _line 34_, I read all the images names of of the current subject being traversed and on _line 39-66_ I traverse those images one by one. On _line 53-54_ I am using OpenCV's `imshow(window_title, image)` along with OpenCV's `waitKey(interval)` method to display the current image being traveresed. The `waitKey(interval)` method pauses the code flow for the given interval (milliseconds), I am using it with 100ms interval so that we can view the image window for 100ms. On _line 57_, I detect face from the current image being traversed. 
-# 
-# **(step-4)** On _line 62-66_, I add the detected face and label to their respective vectors.
-
-# But a function can't do anything unless we call it on some data that it has to prepare, right? Don't worry, I have got data of two beautiful and famous celebrities. I am sure you will recognize them!
-# 
-# ![training-data](visualization/tom-shahrukh.png)
-# 
-# Let's call this function on images of these beautiful celebrities to prepare data for training of our Face Recognizer. Below is a simple code to do that.
-
-# In[5]:
-
-#let's first prepare our training data
-#data will be in two lists of same size
-#one list will contain all the faces
-#and other list will contain respective labels for each face
-print("Preparing data...")
 faces, labels = prepare_training_data("training-data")
 print("Data prepared")
 
@@ -229,17 +217,6 @@ print("Total labels: ", len(labels))
 
 # This was probably the boring part, right? Don't worry, the fun stuff is coming up next. It's time to train our own face recognizer so that once trained it can recognize new faces of the persons it was trained on. Read? Ok then let's train our face recognizer. 
 
-# ### Train Face Recognizer
-
-# As we know, OpenCV comes equipped with three face recognizers.
-# 
-# 1. EigenFace Recognizer: This can be created with `cv2.face.createEigenFaceRecognizer()`
-# 2. FisherFace Recognizer: This can be created with `cv2.face.createFisherFaceRecognizer()`
-# 3. Local Binary Patterns Histogram (LBPH): This can be created with `cv2.face.LBPHFisherFaceRecognizer()`
-# 
-# I am going to use LBPH face recognizer but you can use any face recognizer of your choice. No matter which of the OpenCV's face recognizer you use the code will remain the same. You just have to change one line, the face recognizer initialization line given below. 
-
-# In[6]:
 
 #create our LBPH face recognizer 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -297,10 +274,16 @@ def draw_text(img, text, x, y):
 #and draws a rectangle around detected face with name of the 
 #subject
 def predict(test_img):
+    global familyID
     #make a copy of the image as we don't want to chang original image
     img = test_img.copy()
     #detect face from the image
     face, rect = detect_face(img)
+    if face is None :
+        print("对不起，没有识别到人脸")
+        return img;
+    
+    
 
     cv2.imshow("face",face)
     cv2.waitKey(1000)
@@ -311,6 +294,7 @@ def predict(test_img):
     print("label,confidence",label,confidence)
     #get name of respective label returned by face recognizer
     label_text = subjects[label]
+    familyID=label
     
     #draw a rectangle around face detected
     draw_rectangle(img, rect)
@@ -326,29 +310,42 @@ def predict(test_img):
 print("Predicting images...")
 
 #load test images
-test_img1 = cv2.imread("test-data/test1.jpg")
-test_img2 = cv2.imread("test-data/test2.jpg")
-test_img3 = cv2.imread("test-data/test3.jpg")
+test_img1 = cv2.imread("test-data/face.jpg")
 
-#perform a prediction
 predicted_img1 = predict(test_img1)
-predicted_img2 = predict(test_img2)
-predicted_img3 = predict(test_img3)
 
 
 print("Prediction complete")
 
 #display both images
 cv2.imshow(subjects[1], cv2.resize(predicted_img1, (400, 500)))
-cv2.imshow(subjects[2], cv2.resize(predicted_img2, (400, 500)))
-cv2.imshow(subjects[3], cv2.resize(predicted_img3, (400, 500)))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
 cv2.destroyAllWindows()
+if 1<=today.month and today.month<5:
+    print("春天")
+    season=0
+if 5<=today.month and today.month<7:
+    print("夏天")
+    season=1
+if 7<=today.month and today.month<10:
+    print("秋天")
+    season=2
+if 10<=today.month and today.month<12:
+    print("冬天")
+    season=3
+if"雨" in weather:
+    rain = True
+else:
+    rain = False
+
+print("你好客户，",familyID)
+print("现在是",season,"季")
+print("今天有雨吗",rain)
 
 
 
-
+ 
 
